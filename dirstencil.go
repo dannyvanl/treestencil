db929de40363290697e354b00ca079d2c36e4c2f
Danny <45689672+dannyvanl@users.noreply.github.com>
2024-08-02 13:57:07 +0200

    feat: read config version from config file

--- dirstencil.go
+++ dirstencil.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type configuration struct {
+	Version int64 `yaml:"version"`
+}
+
+func loadConfig(configFile string) configuration {
+	file, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Fatal("Failed to read configuration from ", configFile, err)
+	}
+	var conf configuration
+	err = yaml.Unmarshal(file, &conf)
+	if err != nil {
+		log.Fatal("Failed to unmarshall configuraton from ", configFile, err)
+	}
+	return conf
+}
 
 func main() {
-	fmt.Println("Hello")
+	configFile := "dirstencil.yaml"
+	log.Println("Loading config from", configFile)
+	conf := loadConfig(configFile)
+	log.Println("Found configuration with version", conf.Version)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cogbin/dirstencil
 
 go 1.22.5
+
+require gopkg.in/yaml.v2 v2.4.0
