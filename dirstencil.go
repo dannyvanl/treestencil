6564381c9f5848071f8de90f6c8b76fc3fc52ac6
Danny <45689672+dannyvanl@users.noreply.github.com>
2024-08-02 15:20:19 +0200

    feat: recursively process template dir and subdirs and create matching target dirs

--- dirstencil.go
+++ dirstencil.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path"
 	"sync"
 
 	"gopkg.in/yaml.v2"
@@ -34,10 +35,28 @@ func loadConfig(configFile string) configuration {
 
 func processTarget(name string, t target, templateDir string) {
 	log.Println("Processing target", name)
+	processTemplateDirForTarget(name, t, templateDir, "")
+}
 
-	log.Println("Ensuring dir", t.Dir, "exists for", name)
-	if err := os.MkdirAll(t.Dir, os.ModePerm); err != nil {
-		log.Fatalf("Failed to ensure existence of dir %s: %s", t.Dir, err)
+func processTemplateDirForTarget(name string, t target, baseDir string, subDir string) {
+	targetDir := path.Join(t.Dir, subDir)
+	log.Println("Ensuring dir", targetDir, "exists for", name)
+	if err := os.MkdirAll(targetDir, os.ModePerm); err != nil {
+		log.Fatalf("Failed to ensure existence of dir %s for %s: %s", targetDir, name, err)
+	}
+
+	dir := path.Join(baseDir, subDir)
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Failed to read dir %s for %s: %s", dir, name, err)
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			processTemplateDirForTarget(name, t, baseDir, path.Join(subDir, entry.Name()))
+		} else {
+			log.Println("Processing", entry.Name(), "in", dir, "for", name)
+		}
+
 	}
 }
 
